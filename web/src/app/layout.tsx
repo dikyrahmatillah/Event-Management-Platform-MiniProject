b77c52fd6bef20cef92938d8ec9ed0306eae68ef
yamilham <yamilham96@gmail.com>
2025-07-22 15:27:25 +0700

    feat: test commit

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat, Rock_Salt } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const rockSalt = Rock_Salt({
+  variable: "--font-rock-salt",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.variable} ${rockSalt.variable} antialiased`}
       >
         {children}
       </body>
