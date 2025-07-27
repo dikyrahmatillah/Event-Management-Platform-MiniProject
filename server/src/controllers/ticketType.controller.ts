0b6ce1d6f13710c80b59c2535b2a4045fd052cbc
Diky Rahmatillah <31226543+dikyrahmatillah@users.noreply.github.com>
2025-07-27 22:51:13 +0700

    feat(ticket): add creation and validation fix(error): handle Prisma unique constraint errors refactor(schema): make ticketType unique per event

---
+++ server/src/controllers/ticketType.controller.ts
@@ -0,0 +1,28 @@
+import { TicketTypeService } from "@/services/ticketType.service.js";
+import { NextFunction, Request, Response } from "express";
+
+export class TicketController {
+  private ticketTypeService = new TicketTypeService();
+
+  createTickets = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const tickets = Array.isArray(request.body)
+        ? request.body
+        : [request.body];
+
+      const eventId = tickets[0].eventId;
+      await this.ticketTypeService.organizerIdMatches(eventId, request.user.id);
+
+      const newTickets = await this.ticketTypeService.createTickets(tickets);
+      return response.status(201).json(newTickets);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export const ticketController = new TicketController();
--- server/src/validations/event.validation.ts
+++ server/src/validations/event.validation.ts
@@ -1,18 +1,32 @@
 import z from "zod";
 
 export const eventSchema = z.object({
-  organizerId: z.number(),
-  eventName: z.string().max(255),
+  organizerId: z.number("Organizer ID must be a number"),
+  eventName: z.string().max(255, "Event name cannot exceed 255 characters"),
   description: z.string().optional(),
   category: z.string().optional(),
-  location: z.string().max(255),
-  price: z.number().nonnegative().optional().default(0),
+  location: z.string().max(255, "Location cannot exceed 255 characters"),
+  price: z
+    .number()
+    .nonnegative("Price must be a non-negative number")
+    .optional()
+    .default(0),
   startDate: z.date().default(() => new Date()),
   endDate: z
     .date()
     .default(() => new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)),
-  totalSeats: z.number().int().nonnegative().optional().default(0),
-  availableSeats: z.number().int().nonnegative().optional().default(0),
+  totalSeats: z
+    .number()
+    .int()
+    .nonnegative("Total seats must be a non-negative integer")
+    .optional()
+    .default(0),
+  availableSeats: z
+    .number()
+    .int()
+    .nonnegative("Available seats must be a non-negative integer")
+    .optional()
+    .default(0),
   imageUrl: z.url().optional(),
   status: z.enum(["ACTIVE", "INACTIVE", "CANCELLED"]).default("ACTIVE"),
 });
