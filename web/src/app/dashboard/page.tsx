ad78f9f8eb323b135176c77e82e10b1345fdcad9
Diky Rahmatillah <31226543+dikyrahmatillah@users.noreply.github.com>
2025-07-31 12:07:21 +0700

    refactor(dashboard): refactor dashboard analytics

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -13,6 +13,7 @@ import { ChartAreaInteractive } from "@/app/dashboard/components/chart-area-inte
 import { RecentTransactions } from "@/app/dashboard/components/recent-transactions";
 import { UpcomingEvents } from "@/app/dashboard/components/upcoming-events";
 import { EventReviews } from "@/app/dashboard/components/event-reviews";
+import { DashboardAnalytics } from "@/app/dashboard/components/dashboard-anayltics";
 
 export default function Page() {
   return (
@@ -38,11 +39,7 @@ export default function Page() {
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <SectionCards />
-
-            <div className="px-4 lg:px-6">
-              <ChartAreaInteractive />
-            </div>
+            <DashboardAnalytics />
 
             <div className="grid grid-cols-1 gap-4 px-4 lg:px-6 xl:grid-cols-4">
               <div className="xl:col-span-2">
