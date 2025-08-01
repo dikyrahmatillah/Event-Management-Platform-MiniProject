a677642a5997a5abdab3b80777581e66dc5ecfdc
Diky Rahmatillah <31226543+dikyrahmatillah@users.noreply.github.com>
2025-08-01 09:52:25 +0700

    fix: fix push error

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -34,15 +34,7 @@ export class AuthController {
 
       response
         .status(201)
-<<<<<<< HEAD
-<<<<<<< HEAD
-        .json({ message: "User registered successfully", user });
-=======
         .json({ message: "User registered successfully", data: user });
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
-=======
-        .json({ message: "User registered successfully", data: user });
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
     } catch (error) {
       next(error);
     }
@@ -52,19 +44,9 @@ export class AuthController {
     try {
       const { email, password } = loginSchema.parse(request.body);
       const accessToken = await this.authService.loginUser(email, password);
-<<<<<<< HEAD
-<<<<<<< HEAD
-      response.status(200).json({ message: "Login successful", accessToken });
-=======
       response
         .status(200)
         .json({ message: "Login successful", data: { accessToken } });
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
-=======
-      response
-        .status(200)
-        .json({ message: "Login successful", data: { accessToken } });
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
     } catch (error) {
       next(error);
     }
@@ -78,15 +60,7 @@ export class AuthController {
     try {
       const userId = Number(request.params.id);
       const userProfile = await this.authService.getPublicProfile(userId);
-<<<<<<< HEAD
-<<<<<<< HEAD
-      response.status(200).json(userProfile);
-=======
       response.status(200).json({ data: userProfile });
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
-=======
-      response.status(200).json({ data: userProfile });
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
     } catch (error) {
       next(error);
     }
@@ -100,15 +74,7 @@ export class AuthController {
     try {
       const userId = request.user.id;
       const userProfile = await this.authService.getUserProfile(userId);
-<<<<<<< HEAD
-<<<<<<< HEAD
-      response.status(200).json(userProfile);
-=======
       response.status(200).json({ data: userProfile });
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
-=======
-      response.status(200).json({ data: userProfile });
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
     } catch (error) {
       next(error);
     }
@@ -133,20 +99,10 @@ export class AuthController {
         request.user.id,
         data
       );
-<<<<<<< HEAD
-<<<<<<< HEAD
-      response.status(200).json(result);
-=======
-=======
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
       response.status(200).json({
         message: "Profile updated successfully",
         data: result,
       });
-<<<<<<< HEAD
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
-=======
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
     } catch (error) {
       next(error);
     }
@@ -166,20 +122,10 @@ export class AuthController {
         oldPassword,
         newPassword
       );
-<<<<<<< HEAD
-<<<<<<< HEAD
-      response.status(200).json(result);
-=======
-=======
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
       response.status(200).json({
         message: "Password changed successfully",
         data: result,
       });
-<<<<<<< HEAD
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
-=======
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
     } catch (error) {
       next(error);
     }
@@ -193,20 +139,10 @@ export class AuthController {
     try {
       const { email } = forgotPasswordSchema.parse(request.body);
       const result = await this.authService.sendPasswordReset(email);
-<<<<<<< HEAD
-<<<<<<< HEAD
-      response.status(200).json(result);
-=======
-=======
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
       response.status(200).json({
         message: "Password reset email sent successfully",
         data: result,
       });
-<<<<<<< HEAD
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
-=======
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
     } catch (error) {
       next(error);
     }
@@ -220,20 +156,10 @@ export class AuthController {
     try {
       const { token, newPassword } = resetPasswordSchema.parse(request.body);
       const result = await this.authService.resetPassword(token, newPassword);
-<<<<<<< HEAD
-<<<<<<< HEAD
-      response.status(200).json(result);
-=======
-=======
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
       response.status(200).json({
         message: "Password reset successfully",
         data: result,
       });
-<<<<<<< HEAD
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
-=======
->>>>>>> 17eb489d69d6a62e957f4084671f38a02fef9139
     } catch (error) {
       next(error);
     }
